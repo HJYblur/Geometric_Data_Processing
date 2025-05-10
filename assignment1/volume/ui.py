1da85a32f82d497cdba1856efd18de55f9632aaf
Yang Renyi <jianghurainxwy@gmail.com>
2025-05-10 20:32:23 +0200

    volume, genus

--- assignment1/volume/ui.py
+++ assignment1/volume/ui.py
@@ -14,11 +14,21 @@ class MeshVolume(bpy.types.Panel):
 
     def draw(self, context):
         # TODO: Check that the user has selected a mesh
+        if context.active_object is None:
+            self.layout.label(text="Select a mesh")
+            return
 
         # TODO: Obtain a BMesh from the selected mesh
+        bm = bmesh.new()
+        bm.from_mesh(context.active_object.data)
 
         # TODO: Apply the world transformation to the BMesh (so that scaling with hotkey 's' affects volume)
+        transformed_bm = bm.copy()
+
+        matrix_world = context.active_object.matrix_world
+        for v in transformed_bm.verts:
+            v.co = matrix_world @ v.co
 
         # TODO: Show the computed volume using a label
-
-        pass
+        volume = mesh_volume(transformed_bm)
+        self.layout.label(text=f"Volume: {volume:.3f}")
